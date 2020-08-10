91993ff74093f5dd27c1ec56551bf43392a6aaf8
zli <zjli.mail@gmail.com>
2020-08-10 12:56:22 +0200

    Extract bounding boxes from output

--- custom/extract_bboxes.py
+++ custom/extract_bboxes.py
@@ -1,23 +1,25 @@
+from yolo_result_parsing import try_parse_bb
+
 image_path_token = 'Enter Image Path:'
 person_token = 'person:'
 
-# lines - a list of strings
-def try_add_one_detecton(lines):
-    patches = []
+
+def try_add_patch(lines, img_patches):
     if len(lines) <= 1:
-        return patches
+        return
     else:
         line = lines[0]
         if not line.startswith(image_path_token):
             raise Exception(f'Expected: {image_path_token}; Actual: {line}')
 
         token_len = len(image_path_token)
-        img_path = line[len(token_len): line.find(':', token_len) - token_len]
+        img_path = line[token_len: line.find(':', token_len)]
 
         for i in range(1, len(lines)):
-            line = lines[i]
-            if not line.startswith(person_token):
-                raise Exception(f'Expected: {person_token}; Actual: {line}')
+            bb = try_parse_bb(lines[i], 50)
+            if bb != None:
+                bb['img'] = img_path
+                img_patches.append(bb)
 
 
 def extract_bboxes(results, object_name='person', threshold_percent='50'):
@@ -31,18 +33,25 @@ def extract_bboxes(results, object_name='person', threshold_percent='50'):
         Enter Image Path: /home/zli/data/hospital/ChieriCorridor/1/1.jpg: Predicted in 26.807000 milli-seconds.
         Enter Image Path: /home/zli/data/hospital/ChieriCorridor/1/13.jpg: Predicted in 32.587000 milli-seconds.
         person: 97%	(left_x: 1234   top_y:   70   width:   91   height:  236)
-    threshold_percent - [0, 100]"""
-    batch = []
+    threshold_percent - [0, 100]
+    """
+    batch_lines = []
+    img_patches = []
     for line in results:
         if line.startswith(image_path_token):
-            if len(batch) > 0:
-                try_add_one_detecton(batch)
-                batch.clear()
+            if len(batch_lines) > 0:
+                try_add_patch(batch_lines, img_patches)
+                batch_lines.clear()
 
-            batch.append(line)
+            batch_lines.append(line)
         elif line.startswith(person_token):
-            batch.append(line)
+            batch_lines.append(line)
+
+    print(img_patches)
+    print('done')
+
 
 if __name__ == '__main__':
-    result_file = open('/home/zli/data/hospital/ChieriCorridor/1/result.txt', 'r')
+    result_file = open(
+        '/home/zli/data/hospital/ChieriCorridor/1/result.txt', 'r')
     extract_bboxes(result_file.readlines())
