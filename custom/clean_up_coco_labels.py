1b3c35f5a4c1188e338e775ba51c297e49a0c400
zli <zli@mail.com>
2020-12-20 19:30:31 +0100

    1

--- custom/clean_up_coco_labels.py
+++ custom/clean_up_coco_labels.py
@@ -1,5 +1,6 @@
 import os
 import os.path as osp
+from random import randrange
 
 import coco_fileutils
 from coco_fileutils import get_file_list
@@ -52,14 +53,37 @@ def files_with_large_enough_boxes(image_dir, count_to_extract):
     return list(map(lambda f: f['filepath'], good_files))
 
 
-def copy_files_with_with_large_enough_boxes(src_image_dir, dest_image_dir):
-    filenames = files_with_large_enough_boxes(src_image_dir, 30000)
+def files_without_labels(image_dir):
+    target_files = []
+    filenames = get_file_list(image_dir, True, '.jpg')
+
+    for filename in filenames:
+        label_filename = coco_fileutils.filename_change_ext(filename, '.txt')
+        if osp.isfile(label_filename) and len(coco_fileutils.read_bounding_boxes(label_filename)) == 0:
+            target_files.append(filename)
+
+    return target_files
+
+
+def copy_files_with_with_large_enough_boxes(src_image_dir, dest_image_dir, count_to_extract):
+    filenames = files_with_large_enough_boxes(src_image_dir, count_to_extract)
     coco_fileutils.copy_files(filenames, dest_image_dir)
 
 
-if __name__ == "__main__":
-    copy_files_with_with_large_enough_boxes(user_path('Data/Coco/images/train2017'),
-                                            user_path('Data/Coco/images/train'))
+def randomly_select_labelless_images(src_image_dir, select_count):
+    filenames = files_without_labels(src_image_dir)
+    file_count = len(filenames)
+
+    selected_filenames = []
+    for i in range(select_count):
+        selected_filenames.append(filenames[randrange(file_count)])
+
+    return selected_filenames
+
+
+if __name__ == "__main__":    
+    # filenames = randomly_select_labelless_images(user_path('Data/Coco/images/train2017'), 2500)
+    # coco_fileutils.copy_files(filenames, '/home/zli/Data/Coco/images/train/')
     # write_labelled_images_filenames(
     #     user_path('Data/Coco/images/train2017'), user_path('/home/zli/Data/Coco/images/train_images.txt'))
 
